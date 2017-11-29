5f8a83cb31578b6d7ffb2d0c96e670bde8fb1d35
xyc <739759506@qq.com>
2017-11-29 12:51:02 +0800

    增加左右切换功能
    代码冗余，需优化

--- js/index.js
+++ js/index.js
@@ -1,42 +1,30 @@
 window.addEventListener('load', function() {
 	addImgSlide();
-	function addImgSlide() {
-		var aImg = document.getElementsByTagName('img');
-		var len = aImg.length;
-		// var arr = [];
-		// for(var i = 0; i < len; i++) {
-		// 	if (aImg[i].getAttribute('data-imggroup')){
-		// 		arr.push(aImg[i]);
-		// 	}
-		// }
-		// var json = {};
-		// var newLen = arr.length;
-		// for (var j = 0; j < newLen; j++) {
-		// 	if (!json[arr[j].getAttribute('data-imggroup')]) {
-		// 		json[arr[j].getAttribute('data-imggroup')] = [];
-		// 		json[arr[j].getAttribute('data-imggroup')].push(arr[j]);
-		// 	} else {
-		// 		json[arr[j].getAttribute('data-imggroup')].push(arr[j]);
-		// 	}
-		// }
-		var json = {};
-		for (var i = 0; i < len; i++) {
-			if (aImg[i].getAttribute('data-imggroup')) {
-				if (!json[aImg[i].getAttribute('data-imggroup')]) {
-					json[aImg[i].getAttribute('data-imggroup')] = [];
-					json[aImg[i].getAttribute('data-imggroup')].push(aImg[i]);
-				} else {
-					json[aImg[i].getAttribute('data-imggroup')].push(aImg[i]);
-				}
+	
+
+
+}, false); 
+
+function addImgSlide() {
+	var aImg = document.getElementsByTagName('img');
+	var len = aImg.length;
+	var json = {};
+	for (var i = 0; i < len; i++) {
+		var imgData = aImg[i].getAttribute('data-imggroup');
+		if (imgData) {
+			if (!json[imgData]) {
+				json[imgData] = [];
+				json[imgData].push(aImg[i]);
+			} else {
+				json[imgData].push(aImg[i]);
 			}
 		}
-		console.log(json);
-		var frame = new PowerPoint(json);
-		
 	}
-
-
-}, false);
+	var frame = new PowerPoint(json);
+	document.addEventListener('resize', function() {
+		frame.resize(document.body);
+	}, false);
+}
 
 function PowerPoint(json) {
 	this.opts = json;
@@ -55,12 +43,67 @@ PowerPoint.prototype.addEvent = function() {
 			for(var i = 0; i < len; i++) {
 				(function(i) {
 					_this.opts[k][i].addEventListener('click', function() {
-						_this.createFrame(this);
+						var that = this;
+						_this.createFrame(that);
+						document.addEventListener('keyup', function(event) {	//按下键盘Esc键，关闭详图
+							if (event.keyCode == 27) {
+								_this.frameClose();
+							}
+						},false);
 
+						_this.oPrev.addEventListener('click', function() {
+							var oldImg = document.getElementById('oldImg');
+							_this.oFrame.removeChild(oldImg);
+							i--;
+							if ( i <= 0) {
+								i = 0;
+								this.style.display = 'none';
+							}
+							_this.oNext.style.display = 'block';
+
+							var curImg = _this.opts[k][i].cloneNode(true)
+							curImg.setAttribute('id', 'oldImg');
+							_this.oFrame.appendChild(curImg);
+							var imgMessage = curImg.getAttribute('alt');
+							_this.addDescription(imgMessage, i + 1, len)
+						}, false);
+
+						_this.oNext.addEventListener('click', function() {
+							var oldImg = document.getElementById('oldImg');
+							_this.oFrame.removeChild(oldImg);
+							i++;
+							if ( i >= len-1) {
+								i = len-1;
+								this.style.display = 'none';
+							}
+							_this.oPrev.style.display = 'block';
+
+							var curImg = _this.opts[k][i].cloneNode(true)
+							curImg.setAttribute('id', 'oldImg');
+							_this.oFrame.appendChild(curImg)
+							var imgMessage = curImg.getAttribute('alt');
+							_this.addDescription(imgMessage, i + 1, len)
+
+						}, false);
+
+						document.addEventListener('click', function(event) {
+							var oPrevWidth = _this.oPrev.offsetWidth,
+								oFrameHeight = _this.oFrame.offsetHeight,
+								oFrameWidth = _this.oFrame.offsetWidth,
+								oFrameLeft = _this.oFrame.offsetLeft- oFrameWidth / 2,
+								oFrameRight = oFrameLeft + oFrameWidth;
+								oFrameTop = _this.oFrame.offsetTop - oFrameHeight / 2;
+							if (event.clientX < oFrameLeft || event.clientX > oFrameRight || event.clientY < oFrameTop || event.clientY > (oFrameTop + oFrameHeight)) {
+								// if(_this.oFrame){
+								// 	_this.frameClose();
+								// }
+							}
+							
+						}, false);
+			
 						_this.oClose.addEventListener('click', function() {	//关闭
 							_this.frameClose();
 						}, false);
-
 						var imgMessage = this.getAttribute('alt');
 						_this.addDescription(imgMessage, i + 1, len);
 					}, false);
@@ -74,28 +117,55 @@ PowerPoint.prototype.addEvent = function() {
 
 PowerPoint.prototype.createFrame = function(obj) {	//创建图片详情方法
 	this.oScreen = document.createElement('div');	//创建屏幕遮罩
-	this.oFrame = document.createElement('div');	//创建显示框
-	this.oPrev = document.createElement('button');	//创建上一张及下一张按钮
-	this.oNext = document.createElement('button');	
-	this.oClose = document.createElement('a');		//创建关闭按钮
-	this.pageNum = document.createElement('p');		//页码信息
-	this.imgMessage = document.createElement('p');	//图片描述信息
-	var newObj = obj.cloneNode(true);				//复制传入的对象
+	this.oScreen.setAttribute('class', 'screen');	//设置class，用于css
 
-	this.oScreen.setAttribute('class', 'screen');	//设置各个的class，用于css
+	this.oFrame = document.createElement('div');	//创建显示框
 	this.oFrame.setAttribute('class', 'frame');
+
+	this.oPrev = document.createElement('button');	//创建上一张及下一张按钮
+	this.oPrev.setAttribute('class', 'prev');
+	this.oPrev.innerHTML = '<';	
+
+	this.oNext = document.createElement('button');
+	this.oNext.setAttribute('class', 'next');
+	this.oNext.innerHTML = '>';	
+
+	this.oClose = document.createElement('a');		//创建关闭按钮
 	this.oClose.setAttribute('class', 'close');
-	this.imgMessage.setAttribute('class', 'message');
+	this.oClose.innerHTML = '×';
+
+	this.pageNum = document.createElement('p');		//页码信息
 	this.pageNum.setAttribute('class', 'pagenum');
 
-	this.oClose.innerHTML = '×';					//创建的按钮及信息添加到显示框中
+	this.imgMessage = document.createElement('p');	//图片描述信息
+	this.imgMessage.setAttribute('class', 'message');
+
+	var newObj = obj.cloneNode(true);				//复制传入的对象
+	newObj.setAttribute('id', 'oldImg');
+
 	this.oFrame.appendChild(this.imgMessage);
 	this.oFrame.appendChild(this.pageNum);
-	this.oFrame.appendChild(newObj);
 	this.oFrame.appendChild(this.oClose);
+	this.oFrame.appendChild(this.oPrev);
+	this.oFrame.appendChild(this.oNext);
+	this.oFrame.appendChild(newObj);
+	
 
 	document.body.appendChild(this.oFrame);			//将显示框添加到body
 	document.body.appendChild(this.oScreen);		//屏幕遮罩添加到body
+};
+
+PowerPoint.prototype.createBtn = function() {
+	this.oPrev = document.createElement('button');	//创建上一张及下一张按钮
+	this.oPrev.setAttribute('class', 'prev');
+	this.oPrev.innerHTML = '<';	
+
+	this.oNext = document.createElement('button');
+	this.oNext.setAttribute('class', 'next');
+	this.oNext.innerHTML = '>';	
+
+	this.oFrame.appendChild(this.oPrev);
+	this.oFrame.appendChild(this.oNext);
 };
 
 PowerPoint.prototype.frameClose = function() {
@@ -104,10 +174,17 @@ PowerPoint.prototype.frameClose = function() {
 };
 
 PowerPoint.prototype.addDescription = function(str, index, len) {
-		this.imgMessage.innerHTML = str;						//图片信息添加内容
-		this.pageNum.innerHTML = index + " / " +  len;		//页码
+	this.imgMessage.innerHTML = str;						//图片信息添加内容
+	this.pageNum.innerHTML = index + " / " +  len;		//页码
 };
-
-PowerPoint.prototype.go = function(n) {
-
+PowerPoint.prototype.resize = function(obj) {
+	this.oScreen.style.width = obj.offsetWidth + 'px';
+	this.oScreen.style.height = obj.offsetHeight + 'px';
+	this.oFrame.style.maxWidth = obj.offsetWidth * 0.9 + 'px';
+	this.oFrame.style.maxHeight = obj.offsetHeight * 0.9 + 'px';
+}
+PowerPoint.prototype.move = function(obj) {
+	 nthis.frameClose();
+	this.createFrame()
+	
 }
